a0ae16eeead327055bc917feb2c9ce7c8f518743
waelmw <Mohamed505.mw@gmail.com>
2024-02-04 11:56:27 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PorfolioComponent } from './porfolio/porfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:MainComponent},
+  {path:"about",component:AboutComponent,title:"about"},
+  {path:"portfolio",component:PorfolioComponent,title:"portfolio"},
+  {path:"contact",component:ContactComponent,title:"contact"},
+  {path:"**",component:NotFoundComponent,title:"PageNotFound"}
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes ,{scrollPositionRestoration:'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { PorfolioComponent } from './porfolio/porfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    PorfolioComponent,
+    ContactComponent,
+
+    MainComponent,
+     NotFoundComponent,
   ],
   imports: [
     BrowserModule,
